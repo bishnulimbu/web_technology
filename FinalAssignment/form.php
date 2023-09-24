8b1e3f2f670ff7f9cf8ad345c86b7bef2508a1bd
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-24 17:53:43 +0545

    dfasd

---
+++ FinalAssignment/form.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <fieldset>
+      <legend>UpdatedForm</legend>
+      <form>
+        <label for="id">Choose ID:</label><br>
+        <label for="name">UserName :</label><br>
+        <input type="password" name="password"><br>
+        <label for="password">Password :</label><br>
+        <input type="password" name="password"><br>
+        <label for="password">Confirm Password :</label><br>
+        <input type="password" name="password"><br>
+        <label for="email">Email :</label><br>
+        <input type="email" name="email"><br>
+          <input type="submit" value="Submit">
+      </form>
+
+
+      </fieldset>
+
+
+
+
+  </body>
+</html>
